6107534f65308ad5ee5871bee5573ebf223f75b7
Youcef <37760798+YoucefNabil@users.noreply.github.com>
2024-10-22 17:59:45 +0100

    fixes

--- SPriest.lua
+++ SPriest.lua
@@ -8,9 +8,9 @@ local function castduration(spellID)
 	local tempvar = (select(7,GetSpellInfo(spellID)))
 	return tempvar and tempvar/1000 or 0
 end
-local exeOnUnload = function(
-end)
-local exeOnLoad = function( --==================== loading stuff for this profile specifically
+local exeOnUnload = function()
+end
+local exeOnLoad = function() --==================== loading stuff for this profile specifically
 	--==================== Manual Button Hooks (this does 2 things: the first and seventh button in your action bars starts/stops the rotation. Casting a macro or a spell pauses the rotation to let the user cast stuff)
 	_A.pressedbuttonat = 0
 	_A.buttondelay = 0.6
@@ -54,7 +54,7 @@ local exeOnLoad = function( --==================== loading stuff for this profil
 			return true
 		else
 		return false
-	end
+		end
 	end
 	--==================== Events and event related
 	_A.mindflaytb = {}
@@ -137,7 +137,8 @@ local exeOnLoad = function( --==================== loading stuff for this profil
 			table.sort( tempTable, function(a,b) return (a.isplayer > b.isplayer) or (a.isplayer == b.isplayer and a.health < b.health) end )
 		end
 		return tempTable[num] and tempTable[num].guid
-	end)
+	end
+	)
 	_A.FakeUnits:Add('lowestEnemyInSpellRangeExecute', function(num, spell)
 		local tempTable = {}
 		for _, Obj in pairs(_A.OM:Get('EnemyCombat')) do
@@ -153,8 +154,9 @@ local exeOnLoad = function( --==================== loading stuff for this profil
 			table.sort( tempTable, function(a,b) return (a.isplayer > b.isplayer) or (a.isplayer == b.isplayer and a.health < b.health) end )
 		end
 		return tempTable[num] and tempTable[num].guid
-	end)
-end)
+	end
+	)
+end
 --==================== Rotation Table
 local YSP = {
 	--=========================
@@ -214,66 +216,66 @@ local YSP = {
 			local target = Object("target")
 			if target and target:enemy() and target:spellrange("Mind Flay") and target:infront() and target:los() then return target:cast("Mind Flay") end
 		end
-	end,
-	--=========================
-	--========================= AOE/filler
-	--=========================
-	shadowword_pain_any = function()
-		if not player:ischanneling("Mind Flay") or #_A.mindflaytb>=2 then
-			local lowest = Object("lowestEnemyInSpellRangeDOT(Shadow Word: Pain)")
-			if lowest then return lowest:cast("Shadow Word: Pain") end
+		end,
+		--=========================
+		--========================= AOE/filler
+		--=========================
+		shadowword_pain_any = function()
+			if not player:ischanneling("Mind Flay") or #_A.mindflaytb>=2 then
+				local lowest = Object("lowestEnemyInSpellRangeDOT(Shadow Word: Pain)")
+				if lowest then return lowest:cast("Shadow Word: Pain") end
+			end
+		end,
+		shadowword_execute_any = function()
+			if not player:ischanneling("Mind Flay") or #_A.mindflaytb>=2 then
+				local lowest = Object("lowestEnemyInSpellRangeExecute(Shadow Word: Death)")
+				if lowest then return lowest:cast("Shadow Word: Death") end
+			end
+		end,
+		}
+		--==================== Running the rotation
+		local inCombat = function()
+			if not player then player = Object("player") end
+			if not player:ischanneling("Mind Flay") and #_A.mindflaytb>0 then _A.mindflaytb = {} end -- clean mindflay tick table when not casting
+			--=============Debugging section
+			--=============
+			if _A.buttondelayfunc()  then return end -- pauses when pressing spells manually
+			--============= Single Target Main rotation
+			if not _A.modifier_shift() then -- holding shift skips this
+				YSP.shadowform_stance()
+				YSP.vampiric_touch()
+				YSP.shadowword_pain()
+				YSP.mindblast()
+				YSP.deathspell()
+				YSP.mindflay()
+			end
+			--============= Leveling
+			if player:level()<=20 then
+				YSP.smite()
+			end
+			--============= AOE fill
+			YSP.shadowword_execute_any()
+			YSP.shadowword_pain_any()
 		end
-	end,
-	shadowword_execute_any = function()
-		if not player:ischanneling("Mind Flay") or #_A.mindflaytb>=2 then
-			local lowest = Object("lowestEnemyInSpellRangeExecute(Shadow Word: Death)")
-			if lowest then return lowest:cast("Shadow Word: Death") end
-		end
-	end,
-}
---==================== Running the rotation
-local inCombat = function()
-	if not player then player = Object("player") end
-	if not player:ischanneling("Mind Flay") and #_A.mindflaytb>0 then _A.mindflaytb = {} end -- clean mindflay tick table when not casting
-	--=============Debugging section
-	--=============
-	if _A.buttondelayfunc()  then return end -- pauses when pressing spells manually
-	--============= Single Target Main rotation
-	if not _A.modifier_shift() then -- holding shift skips this
-		YSP.shadowform_stance()
-		YSP.vampiric_touch()
-		YSP.shadowword_pain()
-		YSP.mindblast()
-		YSP.deathspell()
-		YSP.mindflay()
-	end
-	--============= Leveling
-	if player:level()<=20 then
-		YSP.smite()
-	end
-	--============= AOE fill
-	YSP.shadowword_execute_any()
-	YSP.shadowword_pain_any()
-end
-
-local spellIds_Loc = {
-}
-
-local blacklist = {
-}
-
-_A.CR:Add("Priest", {
-	name = "Youcef's Shadow Priest",
-	ic = inCombat,
-	ooc = inCombat,
-	use_lua_engine = true,
-	gui = GUI,
-	gui_st = {title="CR Settings", color="87CEFA", width="315", height="370"},
-	wow_ver = "3.3.5",
-	apep_ver = "1.1",
-	-- ids = spellIds_Loc,
-	-- blacklist = blacklist,
-	-- pooling = false,
-	load = exeOnLoad,
-	unload = exeOnUnload
-})							
+		
+		local spellIds_Loc = {
+		}
+		
+		local blacklist = {
+		}
+		
+		_A.CR:Add("Priest", {
+			name = "Youcef's Shadow Priest",
+			ic = inCombat,
+			ooc = inCombat,
+			use_lua_engine = true,
+			gui = GUI,
+			gui_st = {title="CR Settings", color="87CEFA", width="315", height="370"},
+			wow_ver = "3.3.5",
+			apep_ver = "1.1",
+			-- ids = spellIds_Loc,
+			-- blacklist = blacklist,
+			-- pooling = false,
+			load = exeOnLoad,
+			unload = exeOnUnload
+		})									
